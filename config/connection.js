01338f2c376905600f8e92efb5eaf9cc9a7375e7
Daisy Lien <daisylien1211@gmail.com>
2022-11-10 19:02:41 -0800

    change

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const {connect, connection} = require('mongoose');
 
 const connectionString = 
-    process.env.MONGODB_URI || 
+    process.env.MONGODB_URI || `mongodb://localhost:27017/userData`;
 
 connect(connectionString, {
     useNewUrlParser: true,
